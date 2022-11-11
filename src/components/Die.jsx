68acb6f86061a152425c87a21a6d196762ae2912
MCabal77 <marccabal77@gmail.com>
2022-11-11 13:51:21 -0800

    Randomly initialize the dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Die({ value }) {
   return (
-    <div className=" w-20 h-20 bg-white flex items-center justify-center rounded-md shadow-lg">
-      <p className=" font-bold text-xl ">{value}</p>
+    <div className=" w-20 h-20 bg-white flex items-center justify-center rounded-xl shadow-lg">
+      <p className=" font-extrabold text-2xl ">{value}</p>
     </div>
   );
 }
--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useState } from "react";
+
+import { getRandomInt } from "../utils/functions";
 import Die from "./Die";
 
 function GameContainer() {
+  const allNewDice = function () {
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push(getRandomInt(1, 6));
+    }
+    return arr;
+  };
+
+  const [allDice, setAllDice] = useState(allNewDice);
+
   return (
-    <div className="bg-game h-full w-full rounded-lg p-20 flex flex-col items-center">
+    // Game container
+    <div className="bg-game h-full w-full rounded-lg p-20 flex flex-col justify-center items-center">
       {/* Header | Info*/}
       <h1 className="text-primary font-bold text-5xl">Tenzies</h1>
       <h3 className="text-secondary font-inter font-normal text-2xl">
@@ -11,19 +25,15 @@ function GameContainer() {
         current value between rolls.
       </h3>
       {/* Container for dice */}
-      <div className="p-16 flex gap-10">
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>
+      <div className="p-16 grid gap-10 grid-cols-5">
+        {allDice.map((element, index) => (
+          <Die key={index + 1} value={element} />
+        ))}
       </div>
       {/* Button */}
+      <button className="bg-button text-white w-44 h-16 rounded-lg font-bold text-2xl">
+        Roll
+      </button>
     </div>
   );
 }
